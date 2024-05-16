a570d6bd6d73ff5e663a3d7f2c2eb728dc78a5ce
rennan.tavares <rennan.tavares48@gmail.com>
2024-05-16 00:11:24 -0300

    Fix: Fixed endpoint to retrieve all users.

--- app/Domain/Cpf/Cpf.php
+++ app/Domain/Cpf/Cpf.php
@@ -22,17 +22,38 @@ class Cpf
     {
         $isValidLength = $this->isValidLength();
         $isValidNumber = $this->isValidNumber();
-
         return $isValidLength && $isValidNumber;
     }
 
     private function isValidLength(): bool
     {
-        return false;
+        $cpf = $this->cpf;
+        $cpf = preg_replace('/\D/', '', $cpf);
+        return strlen($cpf) === 11;
     }
 
     private function isValidNumber(): bool
     {
-        return false;
+        $cpf = $this->cpf;
+
+        $cpf = preg_replace('/\D/', '', $cpf);
+    
+        if (preg_match('/(\d)\1{10}/', $cpf)) {
+            return false;
+        }
+    
+        $sum = 0;
+        for ($i = 0; $i < 9; $i++) {
+            $sum += (10 - $i) * intval($cpf[$i]);
+        }
+        $digit1 = ($sum * 10) % 11;
+    
+        $sum = 0;
+        for ($i = 0; $i < 10; $i++) {
+            $sum += (11 - $i) * intval($cpf[$i]);
+        }
+        $digit2 = ($sum * 10) % 11;
+    
+        return $cpf[9] == $digit1 && $cpf[10] == $digit2;
     }
 }
