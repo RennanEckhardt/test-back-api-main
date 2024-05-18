f526550a3a9586f9cd5bd82d075aba2f1b570406
rennan.tavares <rennan.tavares48@gmail.com>
2024-05-17 22:08:31 -0300

    feat: add UserMemory Functions

--- app/Infra/Memory/UserMemory.php
+++ app/Infra/Memory/UserMemory.php
@@ -1,15 +1,19 @@
 <?php
 
 namespace App\Infra\Memory;
-
+use App\Domain\User\UserDataValidator;
 use App\Domain\User\User;
 use App\Domain\User\UserPersistenceInterface;
+use App\Domain\Cpf\CpfGenerator;
+use Faker;
 
 class UserMemory implements UserPersistenceInterface
 {
+    private Faker\Generator $faker;
+    
     public function create(User $user): void
     {
-
+        
     }
 
     public function isCpfAlreadyCreated(User $user): bool
@@ -48,10 +52,21 @@ class UserMemory implements UserPersistenceInterface
     }
 
     public function find(User $user): User{
+        $this->faker = Faker\Factory::create();
+        $cpf = new CpfGenerator();
+        $user
+            ->setDataValidator(new UserDataValidator())
+            ->setId($this->faker->uuid())
+            ->setName($this->faker->name())
+            ->setEmail($this->faker->email())
+            ->setCpf($cpf->generate())
+        
+        ;
         return $user;
     }
 
     public function softDelete(User $user): bool{
-        return true;
+        $user->setDeletedAt(date('Y-m-d H:i:s'));
+        return false;
     }
 }
